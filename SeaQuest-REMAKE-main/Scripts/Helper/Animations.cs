14176defe0ddc0fe4d05e057bce9135ea9cb5b95
JAOC1303 <jaocs1303@gmail.com>
2025-05-20 19:21:49 -0400

    Add files via upload

---
+++ SeaQuest-REMAKE-main/Scripts/Helper/Animations.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeaQuest.Scripts.Helper;
+/// <summary>
+/// Contains reference to the animation in game
+/// </summary>
+public static class AnimationsNames
+{
+    /// <summary>
+    /// Contains references to the player animations
+    /// </summary>
+    public static class Player
+    {
+        /// <summary>
+        /// Reference to the<c>"default"</c>animation
+        /// </summary>
+        public const string DEFAULT = "default";
+    }
+
+
+    /// <summary>
+    /// Contains references to the bullet animations
+    /// </summary>
+    public static class Bullet
+    {
+        /// <summary>
+        /// Reference to the<c>"default"</c>animation
+        /// </summary>
+        public const string DEFAULT = "default";
+    }
+}
